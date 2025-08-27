2c2d67456098fb5727e6dfe916786467112bf114
Larissaamarante0810 <larissa.amarante0810@gamil.com>
2025-08-27 12:01:19 -0300

    teste

---
+++ perfil.py
@@ -0,0 +1,37 @@
+#Classe que representa um perfil
+class Perfil:
+    def __init__(self,nome,email,senha,nome_da_conta,curtida,id):
+        self.__nome = nome
+        self.__email
+        self.__senha
+        self.__nome_da_conta
+        self.__curtida
+        self.__id
+
+
+class Perfil():
+
+    def get_nome(self):
+        return self.__nome
+    
+    def get_email(self):
+        return self.__email
+    
+    def get_senha(self):
+        return self.__senha
+    
+    def get_nome_da_conta(self):
+        return self.__nome_da_conta
+    
+    def get_curtida(self):
+        return self.__curtida
+    
+    def get_id(self):
+        return self.__id
+    
+
+
+
+
+
+
---
+++ programa.py
@@ -0,0 +1,9 @@
+# Leitura do valor inteiro 
+N = int(input("Digite um numero inteiro:\n"))
+
+# Para cada numero de 1 ate N
+for i in range(1, N + 1):
+    # Primeira linha 
+    print(1, 1**2, 1**3)
+    # Segunda linha 
+    print(1, 1**2 + 1, 1**3 + 1)
